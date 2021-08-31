4decbc532b649982f9c4168444a1e3bf86e31984
Harish Kumar <g.harishkumar1998@gmail.com>
2021-08-31 07:31:28 +0530

    ⬆️ LabelFrame

--- gui.py
+++ gui.py
@@ -10,15 +10,13 @@ from PIL import ImageTk, Image
 # To get the dialog box to open when required
 from tkinter import filedialog
 
-
-import argparse
 import io
 import time
 import numpy as np
 
 from tflite_runtime.interpreter import Interpreter
 
-x='empty.png'
+x='/home/pi/Desktop/bg.jpg'
 
 def load_labels(path):
 	with open(path, 'r') as f:
@@ -50,7 +48,9 @@ def open_img():
 	global x
 	# Select the Imagename from a folder
 	x = openfilename()
+	show_img()
 
+def show_img():
 	# opens the image
 	img = Image.open(x)
 	
@@ -86,45 +86,37 @@ def classify():
 	# print(label_id)
 	print(labels[label_id])
 	print(prob)
-	result.config(text=labels[label_id]+' '+str(round(prob*100,2))+'%')
+	identification.config(text=labels[label_id])
+	confidence.config(text='Confidence '+str(round(prob*100,2))+'%')
 
 # Create a window
 root = Tk()
 
-# Set Title as Image Loader
-root.title("Image Loader")
+# Set Title as Image Classifier
+root.title("Image Classifier")
 
 # Set the resolution of window
-root.geometry("440x400")
+root.geometry("440x420")
 
 # Allow Window to be resizable
 root.resizable(width = True, height = True)
 title = Label(root, text = "Hashan Medicare").grid(row = 0, column = 0, pady = 10, columnspan = 4)
 title1 = Label(root, text = "Image Classifier").grid(row = 1, column = 0, columnspan = 4)
 # Create a button and place it into the window using grid layout
-btn = Button(root, text ='Browse', command = open_img).grid(
-										row = 2, column = 0, padx = 30)
-btn1 = Button(root, text ='Capture', command = capture).grid(
-										row = 3, column = 0, padx = 30)
-btn1 = Button(root, text ='Classify', command = classify).grid(
-										row = 4, column = 0, padx = 30)
-img = Image.open('/home/pi/Desktop/bg.jpg')
+grp = LabelFrame(root, text = "Select Image")
+grp.grid(row = 2, column = 0, rowspan = 2, padx = 20, pady = (35, 0))
+btn = Button(grp, text ='Browse', width = 10, command = open_img).grid(
+										row = 0, column = 0, padx = 10, pady = 30)
+btn1 = Button(grp, text ='Capture', width = 10, command = capture).grid(
+										row = 1, column = 0, padx = 10, pady = 30)
+btn2 = Button(root, text ='Classify', width = 10, command = classify).grid(
+										row = 4, column = 0, padx = 10, pady = 30)
+show_img()
+identification = Label(root, text = "Result", bg = "white", width = 43)
+identification.grid(row = 5, column = 0, padx = 50, columnspan = 5)
 
-# resize the image and apply a high-quality down sampling filter
-img = img.resize((250, 250), Image.ANTIALIAS)
-
-# PhotoImage class is used to add image to widgets, icons etc
-img = ImageTk.PhotoImage(img)
-
-# create a label
-panel = Label(root, image = img)
-
-# set the image as img
-panel.image = img
-panel.grid(row=2, column = 1,columnspan = 3, rowspan = 3, padx = 5, pady = 20)
-
-result = Label(root, text = "Result", bg = "white", width = 43)
-result.grid(row = 5, column = 0, padx = 50, columnspan = 5)
+confidence = Label(root, text = "Confidence", bg = "white", width = 43)
+confidence.grid(row = 6, column = 0, padx = 50, columnspan = 5)
 
 labels = load_labels('labels.txt')
 
